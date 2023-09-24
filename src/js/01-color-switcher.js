f3edb9c13eb942563a177aef0691eed3469cedff
liddiia <liddabojko@gmail.com>
2023-09-24 08:59:38 +0300

    summury

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBt = document.querySelector('[data-start]');
+const stopBt = document.querySelector('[data-stop]');
+const bodyChangeColor = document.querySelector('body');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+bodyChangeColor.style.paddingLeft = '300';
+
+let intervalId = null;
+
+stopBt.setAttribute('disabled', '');
+
+startBt.addEventListener('click', elt => {
+  elt.target.setAttribute('disabled', true);
+  stopBt.removeAttribute('disabled');
+  intervalId = setInterval(() => {
+    bodyChangeColor.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+stopBt.addEventListener('click', elt => {
+  elt.target.setAttribute('disabled', true);
+  startBt.removeAttribute('disabled');
+
+  clearInterval(intervalId);
+});
